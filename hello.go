016132e7bc976dc099d194ac0887548df0e52b0a
Maxine <mwandamaxyine@gmail.com>
2020-10-19 09:33:00 +0300

    Made changes

--- hello.go
+++ hello.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"time"
+	"encoding/json"
 	"github.com/streadway/amqp"
+	"log"
+	_ "net/http"
+	"os"
+	"time"
 )
 
 
@@ -13,11 +14,22 @@ import (
 		TimeStamp int64 `json:"time_stamp"`
 	}
 func main () {
+	log.Println("Starting...")
+	var counter = 0
+	_= godotenv.Load()
+
+	channel:= connecttoQueue()
+	defer channel.Close
 
 	var p Payload
+	log.Println("Looks good")
+
 	for{
 		p.TimeStamp = time.Now().Unix()
+		log.Printf("Step %d", counter)
+		go publishToQueue (p, channel)
 		time.Sleep(time.Second ^1)
+		counter ++
 	}
 
 }
@@ -28,37 +40,45 @@ func FailOnError(err error, msg string) {
 	}
 }
 
-func Connection() {
-	conn, err := amqp.Dial("amqp://Maxine:daddy@localhost:5672/")
+func ConnectToQueue() *ampq.Channel {
+	log.Println("Connecting to queue")
+	conn, err := amqp.Dial(os.Getenv("Q_URL"))
 	FailOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+
+	log.Println("Creating a channel")
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	return ch
+}
+
+func PublishToQueue(p Payload, ch *amqp.Channel){
+	queueName:os.Getenv("Q_NAME")
+	jsonByte, _:= json.Marshal(p)
+	log.Println(">>> Publishing", string(jsonByte), "to queue", queueName)
+
+	err:= ch.Publish ("", queueName, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body: jsonByte,
+	})
+	FailOnError(err, "Unable to publish to queue")
+	log.Println("<<< Done")
+}
+
+func ReceiveFromQueue(){
+	log.Println("Connecting to queue")
+	conn, err := amqp.Dial(os.Getenv("Q_URL"))
+	FailOnError(err, "Failed to connect to RabbitMQ")
 
 	ch, err := conn.Channel()
 	FailOnError(err, "Failed to open a channel")
-	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"hello",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	FailOnError(err, "Failed to declare a queue")
-
-	body := json
-	err = ch.Publish(
-		"",
-		q.Name{
-			false,
-			false,
-			ampq.Publishing{
-				ContentType: "json",
-				Body: []byte(body),
-			}
-		)
-	FailOnError("Failed to publish a message")
+	forever:= make(chan bool)
+	go func() {
+		for d:= range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
-
 
